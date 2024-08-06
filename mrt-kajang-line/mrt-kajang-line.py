3e98e1e774fd7b292bb7d5eb8531775b8f5208a6
henz <65068770+henzljw@users.noreply.github.com>
2024-08-07 00:59:04 +0800

    v0.1.1-commit

--- mrt-kajang-line/mrt-kajang-line.py
+++ mrt-kajang-line/mrt-kajang-line.py
@@ -15,6 +15,7 @@ DataFrameConversion = pds.DataFrame(getDatasets)
 # Fill missing values for any column(s) that declared from datasets.py
 DataFrameConversion['station_code'] = DataFrameConversion['station_code'].fillna('N/A')
 DataFrameConversion['station_name'] = DataFrameConversion['station_name'].fillna('N/A')
+DataFrameConversion['working_name'] = DataFrameConversion['working_name'].fillna('N/A')
 DataFrameConversion['provisional_station'] = DataFrameConversion['provisional_station'].fillna('N/A')
 DataFrameConversion['shelved_station'] = DataFrameConversion['shelved_station'].fillna('N/A')
 DataFrameConversion['park_and_ride'] = DataFrameConversion['park_and_ride'].fillna('N/A')
@@ -23,6 +24,7 @@ DataFrameConversion['tourist_attractions'] = DataFrameConversion['tourist_attrac
 DataFrameConversion['hospitals'] = DataFrameConversion['hospitals'].fillna('N/A')
 DataFrameConversion['universities'] = DataFrameConversion['universities'].fillna('N/A')
 DataFrameConversion['government_agencies'] = DataFrameConversion['government_agencies'].fillna('N/A')
+DataFrameConversion['cafes'] = DataFrameConversion['cafes'].fillna('N/A')
 
 # Save as CSV output file format
 DataFrameConversion.to_csv('mrt-kajang-line.csv', index=False)
